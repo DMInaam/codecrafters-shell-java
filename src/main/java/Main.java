41ff7059722804718e7e88e3df63f534eef178e0
DMInaam <muhammedenaam@gmail.com>
2026-06-04 21:20:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine().trim();
-        System.out.println(input+": command not found");
+        while(true){
+            System.out.print("$ ");
+            String input = sc.nextLine().trim();
+            System.out.println(input+": command not found");
+        }
         sc.close();
     }
 }
