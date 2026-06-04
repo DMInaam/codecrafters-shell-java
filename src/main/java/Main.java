5ef1517733da72a0b364ae35d2ac6967f38fa2fb
DMInaam <muhammedenaam@gmail.com>
2026-06-04 21:29:51 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("$ ");
+            if(!sc.hasNextLine()) break;
             String input = sc.nextLine().trim();
             System.out.println(input+": command not found");
         }
-        sc.close();
     }
 }
