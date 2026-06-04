ad01144c962939a169a6055a48b4dfd8958fb2a0
DMInaam <muhammedenaam@gmail.com>
2026-06-04 23:02:26 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -20,11 +21,27 @@ public class Main {
                     case "exit":System.out.println("exit is a shell builtin");break;
                     case "echo":System.out.println("echo is a shell builtin");break;
                     case "type":System.out.println("type is a shell builtin");break;
-                    default:System.out.println(input+": not found");break;
+                    default:
+                        String execPath = findExec(input);
+                        if(execpath != null)
+                            System.out.println(input+" is "+execPath);
+                        else
+                            System.out.println(input+": not found");
+                        break;
                 }
             }
             else
                 System.out.println(input+": command not found");
         }
+    }
+    private static String findExec(String command){
+        String pathEnv = System.getenv("PATH");
+        String[] dirs = pathEnv.split(File.pathSeparator);
+        for(String dir: dirs){
+            File file = new File(dir,command);
+            if(file.exists() && file.isFile() && file.canExecute())
+                return file.getAbsolutePath();
+        }
+        return null;
     }
 }
