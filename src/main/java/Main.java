e8e92adaf665ca36256eee11b91c9e94de91d3aa
DMInaam <muhammedenaam@gmail.com>
2026-06-05 19:55:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
@@ -8,30 +10,48 @@ public class Main {
             System.out.print("$ ");
             if(!sc.hasNextLine())
                 break;
-            String input = sc.nextLine().trim();
-            if(input.startsWith("exit"))
+            String[] input = sc.nextLine().trim().split(" ");;
+            String command = input[0];
+            if(command.startsWith("exit"))
                 System.exit(0);
-            else if(input.startsWith("echo")){
-                input = input.substring(5).trim().replaceAll("\\n","\n");
-                System.out.println(input);
+            else if(command.startsWith("echo")){
+                for(int i=1;i<input.length;i++){
+                    System.out.print(input[i]);
+                    if(i<input.length-1)
+                        System.out.print(" ");
+                }
+                System.out.println();
             }
-            else if(input.startsWith("type")){
-                input = input.substring(5).trim();
-                switch(input){
+            else if(command.startsWith("type")){
+                String msg = input[2];
+                switch(msg){
                     case "exit":System.out.println("exit is a shell builtin");break;
                     case "echo":System.out.println("echo is a shell builtin");break;
                     case "type":System.out.println("type is a shell builtin");break;
                     default:
-                        String execPath = findExec(input);
+                        String execPath = findExec(msg);
                         if(execPath != null)
-                            System.out.println(input+" is "+execPath);
+                            System.out.println(msg+" is "+execPath);
                         else
-                            System.out.println(input+": not found");
+                            System.out.println(msg+": not found");
                         break;
                 }
             }
+            else if(findExec(command) != null){
+                ProcessBuilder pb = new ProcessBuilder(input);
+                try{
+                    Process process = pb.start();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error executing command: "+e.getMessage());
+                }
+            }
             else
-                System.out.println(input+": command not found");
+                System.out.println(command+": command not found");
         }
     }
     private static String findExec(String command){
